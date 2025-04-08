f015780feeaf022c0cae7e725e20b2c4a5420cdd
GergoKovacs0 <c9kovger@vasvari.org>
2025-04-08 12:59:25 +0200

    Add Fa type definition

---
+++ src/types/Fa.ts
@@ -0,0 +1,14 @@
+import Kategoria from './Kategoria';
+
+type Fa = {
+    nev: string;
+    kategoriaId: number;
+    leiras: string;
+    keszlet: number;
+    ar: number;
+    kepUrl: string;
+    id: number;
+    kategoria: Kategoria;
+};
+
+export default Fa;
